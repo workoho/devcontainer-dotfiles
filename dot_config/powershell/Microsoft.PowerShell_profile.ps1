1b4c74f04ed0e0415530f8adeb7e604358ebd1ac
Julian <jpa@workoho.com>
2024-08-01 02:03:40 +0000

    update

--- dot_config/powershell/Microsoft.PowerShell_profile.ps1
+++ dot_config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,8 @@
 # ~/.config/powershell/Microsoft.PowerShell_profile.ps1: executed by the PowerShell console host on startup.
 
-oh-my-posh init pwsh --config "$env:HOME/.cache/oh-my-posh/themes/powerlevel10k_modern.omp.json" | Invoke-Expression
+# Import Terminal related modules
+Import-Module -Name posh-git -ErrorAction SilentlyContinue || (Microsoft.PowerShell.PSResourceGet\Install-PSResource -Name posh-git -Repository PSGallery -TrustRepository -Scope CurrentUser && Import-Module -Name posh-git)
+Import-Module -Name Terminal-Icons -ErrorAction SilentlyContinue || (Microsoft.PowerShell.PSResourceGet\Install-PSResource -Name Terminal-Icons -Repository PSGallery -TrustRepository -Scope CurrentUser && Import-Module -Name Terminal-Icons)
+
+# Enable oh-my-posh theme
+oh-my-posh init pwsh --config "$env:HOME/.cache/oh-my-posh/themes/powerlevel10k_lite.omp.json" | Invoke-Expression
