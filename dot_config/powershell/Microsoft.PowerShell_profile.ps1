88a5d29d75e06dc18109fe06c1dac61a4c2a88ad
Julian <jpa@workoho.com>
2024-08-01 01:49:15 +0000

    fix

--- dot_config/powershell/Microsoft.PowerShell_profile.ps1
+++ dot_config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,3 @@
 # ~/.config/powershell/Microsoft.PowerShell_profile.ps1: executed by the PowerShell console host on startup.
 
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/powerlevel10k_modern.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:HOME/.cache/oh-my-posh/themes/powerlevel10k_modern.omp.json" | Invoke-Expression
--- dot_config/powershell/profile.ps1
+++ dot_config/powershell/profile.ps1
@@ -1,3 +1,1 @@
 # ~/.config/powershell/profile.ps1: executed by all PowerShell hosts on startup.
-
-$env:POSH_THEMES_PATH="$HOME/.config/oh-my-posh/themes"
